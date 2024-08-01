84788b2a01553dbbfd43f2787a179185209b488c
Ma Chengcong <958625996@qq.com>
2024-08-01 23:41:59 +0800

    feat: 项目环境变量设置

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_ENV: string;
+  readonly VITE_API_HOST: string;
+  readonly VITE_LOAD_ALL_ENV: string;
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
