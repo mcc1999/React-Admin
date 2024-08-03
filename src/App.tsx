663e117d57ef3cd6b0600e3d392352b453c52985
Ma Chengcong <958625996@qq.com>
2024-08-03 14:43:35 +0800

    feat: 引入tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      {/* prettier-ignore */}
-      <h1   >React Admin Template</h1>
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <h1 className="h1">React Admin Template</h1>
       <h2>Current Env: {import.meta.env.VITE_APP_ENV}</h2>
       <h2>API HOST: {import.meta.env.VITE_API_HOST}</h2>
       <h2>Var load all env: {import.meta.env.VITE_LOAD_ALL_ENV}</h2>
-    </>
+    </div>
   );
 }
 
